5342049958741825365a7f3cfbef1aebfd8277f0
Roland Canuto <rcanuto@nexient.com>
2019-07-30 15:30:04 -0400

    first commit to repo

---
+++ src/main/java/com/Practice/App.java
@@ -0,0 +1,15 @@
+package com.Practice;
+
+import com.Practice.beginner.Basic;
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        System.out.println(Basic.multiplyByTen(10));
+    }
+}
---
+++ src/main/java/com/Practice/beginner/Basic.java
@@ -0,0 +1,145 @@
+package com.Practice.beginner;
+
+public class Basic {
+	public static int multiplyByTen(int num) {
+        // return num after multiplying it by ten
+        // code here
+        return num * 10;
+    };
+
+    static void subtractFive(int num) {
+        // return num after subtracting five
+        // code here
+    };
+
+    static void areSameLength(String str1, String str2) {
+        // return true if the two strings have the same length
+        // otherwise return false
+        // code here
+    };
+
+    static void areEqual(int x, int y) {
+        // return true if x and y are the same
+        // otherwise return false
+        // code here
+    };
+
+    static void lessThanNinety(int num) {
+        // return true if num is less than ninety
+        // otherwise return false
+        // code here
+    };
+
+    static void greaterThanFifty(int num) {
+        // return true if num is greater than fiftyp
+        // otherwise return false
+        // code here
+    };
+
+    static void add(int x, int y) {
+        // add x and y together and return the value
+        // code here
+    };
+
+    static void subtract(int x, int y) {
+        // subtract y from x and return the value
+        // code here
+    }
+
+    static void divide(int x, int y) {
+        // divide x by y and return the value
+        // code here
+    }
+
+    static void multiply(int x, int y) {
+        // multiply x by y and return the value
+        // code here
+    }
+
+    static void getRemainder(int x, int y) {
+        // return the remainder from dividing x by y
+        // code here
+    }
+
+    static void isEven(int num) {
+        // return true if num is even
+        // otherwise return false
+        // code here
+    }
+
+    static void isOdd(int num) {
+        // return true if num is odd
+        // otherwise return false
+        // code
+    }
+
+    static void square(int num) {
+        // square num and return the new value
+        // code here
+    }
+
+    static void cube(int num) {
+        // cube num and return the new value
+        // code here
+    }
+
+    static void raiseToPower(int num, int exponent) {
+        // raise num to whatever power is passed in as exponent
+        // code here
+    }
+
+    static void roundint(int num) {
+        // round num and return it
+        // code here
+    }
+
+    static void roundUp(int num) {
+        // round num up and return it
+        // code here
+    };
+
+    static void addExclamationPoint(String str) {
+        // add an exclamation point to the end of str and return the new string
+        // 'hello world' -> 'hello world!'
+        // code here
+    }
+
+    static void combineNames(String firstName, String lastName) {
+        // return firstName and lastName combined as one string and separated by a
+        // space.
+        // 'roland canuto'
+        // code here
+    };
+
+    static void getGreeting(String name) {
+        // Take the name string and concatenate other strings onto it so it takes the
+        // following form:
+        // 'Sam' -> 'Hello Sam!'
+        // code here
+    }
+
+    // If you can't remember these area formulas then head over to Google or look at
+    // the test code.
+
+    static void getRectangleArea(int length, int width) {
+        // return the area of the rectangle by using length and width
+        // code here
+    };
+
+    static void getTriangleArea(int base, int height) {
+        // return the area of the triangle by using base and height
+        // code here
+    }
+
+    static void getCircleArea(int radius) {
+        // return the rounded area of the circle given the radius
+        // code here
+    }
+
+    static void getRectangularPrismVolume(int length, int width, int height) {
+        // return the volume of the 3D rectangular prism given the length, width, and
+        // height
+        // code here
+    };
+
+}
